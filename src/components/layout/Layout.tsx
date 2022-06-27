777b64733a8726275bd43f3119f2feda9f4ff68b
Mateo <mateochando@gmail.com>
2022-06-27 14:28:29 +0200

    Move icons and change picture

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -15,19 +15,6 @@ export default function Layout({ children, home }: any) {
         minHeight: "100vh",
       }}
     >
-      <Head>
-        <link
-          rel="icon"
-          href="https://img.icons8.com/color/48/undefined/laptop--v1.png"
-        />
-        <meta name="description" content="Mateo's personal site" />
-        <meta
-          property="og:image"
-          content={`https://img.icons8.com/color/48/undefined/laptop--v1.png`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
       <Grid container justifyContent={"space-between"}>
         <Grid item xs={2}>
           <AppDrawer />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,31 @@ import { ThemeWrapper } from "components/theme/ThemeWrapper";
 import React from "react";
 import mixpanel from "mixpanel-browser";
 import "styles/global.scss";
+import Head from "next/head";
 
 mixpanel.init(process.env.NEXT_PUBLIC_ANALYTICS!, { debug: true });
 
+export const siteTitle = "Mateo";
+
 export default function App({ Component, pageProps }: any) {
   return (
-    <ThemeWrapper>
-      <Component {...pageProps} />
-    </ThemeWrapper>
+    <>
+      <Head>
+        <link
+          rel="icon"
+          href="https://img.icons8.com/color/48/undefined/laptop--v1.png"
+        />
+        <meta name="description" content="Mateo's personal site" />
+        <meta
+          property="og:image"
+          content={`https://riggoch.vercel.app/img/mateo_barcelona.jpeg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <ThemeWrapper>
+        <Component {...pageProps} />
+      </ThemeWrapper>
+    </>
   );
 }
