1175717a158d3e74ad210bd064e262473acc991f
Mateo <mateochando@gmail.com>
2022-06-27 12:15:07 +0200

    Fix loading section

--- src/components/home/PersonalData.tsx
+++ src/components/home/PersonalData.tsx
@@ -1,16 +1,25 @@
-import React, { useContext } from "react";
+import React, { ReactNode, useContext } from "react";
 import { Grid, Skeleton, useTheme } from "@mui/material";
 import { usePersonInformation } from "lib/api";
 import differenceInyears from "date-fns/differenceInYears";
 import { ColorModeContext } from "components/theme/ThemeWrapper";
 import Image from "next/image";
 
+function LoadingText({
+  content,
+  loading,
+}: {
+  content: string | ReactNode;
+  loading: boolean;
+}) {
+  return loading ? <Skeleton variant="text" width={100} /> : <>{content}</>;
+}
+
 export function PersonalData() {
   const { person, isLoading } = usePersonInformation();
   const theme = useTheme();
   const colorMode = useContext(ColorModeContext);
-  if (isLoading) return <Skeleton variant="rectangular" />;
-  return person ? (
+  return (
     <Grid
       container
       alignItems={"center"}
@@ -20,31 +29,52 @@ export function PersonalData() {
       className="font-mono text-center align-middle"
     >
       <Grid item xs={12}>
-        Full name: {person?.name} {person?.surname}
-      </Grid>
-      <Grid item xs={12}>
-        Age: {differenceInyears(new Date(), person?.birthDate)} years old
-      </Grid>
-      <Grid item xs={12}>
-        Birthplace: Córdoba,{" "}
-        <Image
-          alt="Argentina"
-          src="https://purecatamphetamine.github.io/country-flag-icons/3x2/AR.svg"
-          width={30}
-          height={20}
+        <LoadingText
+          loading={isLoading || !person}
+          content={`Full name: ${person?.name} ${person?.surname}`}
         />
       </Grid>
       <Grid item xs={12}>
-        Now: Stockholm,{" "}
-        <Image
-          alt="Sweden"
-          src="https://purecatamphetamine.github.io/country-flag-icons/3x2/SE.svg"
-          width={30}
-          height={20}
+        <LoadingText
+          loading={isLoading || !person}
+          content={` Age: ${differenceInyears(
+            new Date(),
+            person?.birthDate!
+          )} years old`}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <LoadingText
+          loading={isLoading || !person}
+          content={
+            <>
+              Birthplace: Córdoba,{" "}
+              <Image
+                alt="Argentina"
+                src="https://purecatamphetamine.github.io/country-flag-icons/3x2/AR.svg"
+                width={30}
+                height={20}
+              />
+            </>
+          }
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <LoadingText
+          loading={isLoading || !person}
+          content={
+            <>
+              Now: Stockholm,{" "}
+              <Image
+                alt="Sweden"
+                src="https://purecatamphetamine.github.io/country-flag-icons/3x2/SE.svg"
+                width={30}
+                height={20}
+              />
+            </>
+          }
         />
       </Grid>
     </Grid>
-  ) : (
-    <></>
   );
 }
