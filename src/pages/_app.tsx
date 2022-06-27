7a2eaa06d89f26b5e14260d8da0f3effa8bf5eaf
Mateo <mateochando@gmail.com>
2022-06-27 12:48:38 +0200

    Remove app tracj and put analytics in key

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,9 @@ import React from "react";
 import mixpanel from "mixpanel-browser";
 import "styles/global.scss";
 
-mixpanel.init("9a2e1fbb270231f6b2ed1bcf09849de8", { debug: true });
+mixpanel.init(process.env.NEXT_PUBLIC_ANALYTICS!, { debug: true });
 
 export default function App({ Component, pageProps }: any) {
-  mixpanel.track("App");
   return (
     <ThemeWrapper>
       <Component {...pageProps} />
